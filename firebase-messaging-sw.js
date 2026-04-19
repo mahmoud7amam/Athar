664e407e797943b836376476133ea744d9c5c016
mahmoud7amam <mahmoud7amam0m@gmail.com>
2026-04-19 04:38:32 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,8 +1,11 @@
-importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
+  apiKey: "AIzaSyBzaEUrY6MX38n3s7KcIfcx2mhqQbOCfCY",
+  authDomain: "gootff-dcc2a.firebaseapp.com",
   projectId: "gootff-dcc2a",
+  storageBucket: "gootff-dcc2a.firebasestorage.app",
   messagingSenderId: "1027459267887",
   appId: "1:1027459267887:android:ecb608f8a39f58e8bf75da"
 });
